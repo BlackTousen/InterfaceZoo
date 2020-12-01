0fd1148ecca7fa407cd353faca7d6431090a1d68
Jerry Robertson <jerry.r.robertson1@gmail.com>
2020-12-01 13:23:05 -0600

    first commit

---
+++ Interface.cs
@@ -0,0 +1,19 @@
+namespace InterfaceZoo
+{
+    public interface IWalking
+    {
+        void Run();
+        void Walk();
+    }
+
+    public interface ISwimming
+    {
+        int MaximumDepth { get; }
+        void Swim();
+    }
+
+    public interface IFlying
+    {
+        void Fly();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterfaceZoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Pigeon p = new Pigeon();
+            Seagull s = new Seagull();
+
+            List<IFlying> birds = new List<IFlying>() { p, s };
+        }
+    }
+}
